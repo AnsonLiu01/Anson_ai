b80246c1c2423d03f161f3d9c619df2171f4eef2
AnsonLiu01 <ansonkliu@outlook.com>
2025-07-17 18:12:48 +0800

    updating keyword label method via choosing the most optimal keyword option based on best similarity score. Starting work for more coherent labels

--- src/topic_model/label_topics.py
+++ src/topic_model/label_topics.py
@@ -1,5 +1,6 @@
 import sys
 import os
+import re
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 
 from typing import Dict, List
@@ -7,8 +8,9 @@ from typing import Dict, List
 from sentence_transformers import SentenceTransformer
 from loguru import logger
 from keybert import KeyBERT
-import torch
 from sentence_transformers import util
+from transformers import pipeline
+from textblob import TextBlob
 
 
 from src.utils import load_yaml
@@ -50,28 +52,81 @@ class TopicLabeller:
         kw_model = KeyBERT(model='all-MiniLM-L6-v2')
         labels = {}
 
-        for topic_id, doc in topic_words.items():            
+        for topic_id, doc in topic_words.items():
+            label_id = topic_id + 1 if -1 in topic_words.keys() else topic_id           
             doc_embedding = self.sentence_trans.encode(" ".join(val for val in doc), convert_to_tensor=True)
             similarity_scores = {
-                predefined_labels[topic_id + 1]: util.pytorch_cos_sim(
+                predefined_labels[label_id]: util.pytorch_cos_sim(
                 doc_embedding,
-                self.sentence_trans.encode(predefined_labels[topic_id + 1], convert_to_tensor=True)
+                self.sentence_trans.encode(predefined_labels[label_id], convert_to_tensor=True)
             ).item()
             }
             
-            best_label = max(similarity_scores, key=similarity_scores.get)
-            if similarity_scores[best_label] >= threshold:
-                logger.info(f'topic_id {topic_id}: default topic label sufficient ')
-                labels[topic_id] = best_label
+            default_label = max(similarity_scores, key=similarity_scores.get)
+            if similarity_scores[default_label] >= threshold:
+                logger.success(f'topic_id {topic_id}: default topic label sufficient')
+                labels[topic_id] = default_label
             else:
                 logger.info(f'topic_id {topic_id}: default topic label not relevant enough, extracting keywords')
-                keywords = kw_model.extract_keywords(  # TODO: continue from here
+                keywords = kw_model.extract_keywords(
                     doc, 
                     keyphrase_ngram_range=(1, 2), 
-                    stop_words='english', 
+                    stop_words='english',
+                    use_mmr=True,
+                    nr_candidates=20,
                     top_n=2
                 )
-                dynamic_label = ", ".join([kw for kw, _ in keywords])
-                labels[topic_id] = dynamic_label or "Uncategorized"
+                
+                top_label, top_similarity = None, -1
+                
+                for kw_chunk in keywords:
+                    if top_similarity < kw_chunk[0][1]:
+                        top_label, top_similarity = kw_chunk[0][0], kw_chunk[0][1]
+                        logger.debug(f'NEW top label: {top_label} (similarity score: {top_similarity})')
+                
+                top_label = self.refine_label(label=top_label, doc=doc, method='t5')
+                
+                logger.success(f'Using new keyword label for topc id {topic_id}: {top_label}')
+
+                labels[topic_id] = top_label or 'Uncategorised'
 
         return labels
+    
+    @staticmethod
+    def refine_label(
+        label: str,
+        doc: List[str],
+        method: str
+    ) -> str:
+        """
+        Function to refine label to be grammatically correct
+        :param label: label to refine
+        :param doc: main document for additional context
+        :param method: method of refinement
+        :return: grammatically correct  and refined label
+        """
+        logger.info(f'Refining label: {label}')
+        
+        if method == 'textblob':
+            blob = TextBlob(label)
+            refined = blob.correct()
+        elif method == 't5':
+            t5_grammar = pipeline("text2text-generation", model="vennify/t5-base-grammar-correction")
+            
+            prompt = f"""The string '{label}' is the output of a keyword-focused approach for labels for a topic mapping task focused around therapy.\n 
+            Can you correct the grammar or use these keywords to create a coherent and meaningful phrase.\n
+            Below is the document from which these keywords have been obtained from, use this as additional context when
+            creating a short coherent phrase for the label for the current topic\n
+            supporting context document below:\n
+            {' '.join(val for val in doc)}
+            Your output should only include the new refined phrase as the new label
+            """
+            
+            refined = t5_grammar(prompt, max_length=20)[0]['generated_text']
+        else:
+            raise ValueError(f'Method must be one of [textblob, t5], got: {method}')
+        return refined  # TODO: continue without label coherency
+
+        
+        
+        
