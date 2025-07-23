d22e294ddee79cdbf501fc1fdd44b54f94a1d264
AnsonLiu01 <ansonkliu@outlook.com>
2025-07-23 17:32:49 +0800

    adding new eda script

---
+++ src/topic_model/eda_topics.py
@@ -0,0 +1,50 @@
+import os
+
+from loguru import logger
+
+
+class EDATopics:
+    """
+    Class for topic eda
+    """
+    def __init__(self):
+        self.bert = None
+        self.run_eda: bool = None
+    
+    def run_eda_check(func):
+        """
+        Decorator to execute pre-call logic before any function
+        """
+        def function(self, *args, **kwargs):
+            if self.run_eda:
+                logger.success(f"⚡EDA enabled, running function {func.__name__}")
+                print('============= EDA =============\n')
+                result = func(self, *args, **kwargs)
+                print('\n=========== EDA EXIT ==========')
+            else:
+                logger.debug(f'EDA disabled, not running function {func.__name__}')
+            
+            return result
+        return function
+
+    @run_eda_check
+    def get_topic_freq(self) -> None:
+        """
+        Function to get topic frequencies and of which are outliers
+        """
+        topic_freq = self.bert.get_topic_freq()
+        outliers = topic_freq['Count'][topic_freq['Topic'] == -1].iloc[0]
+        
+        print(f'{outliers} documents have not been classified')
+        print(f'The other {topic_freq["Count"].sum() - outliers} documents are {topic_freq["Topic"].shape[0]-1} topics')
+        
+        print(f'All topic frequencies:\n{topic_freq[["Topic", "Count"]]}')
+    
+    @run_eda_check
+    def visual_topic_embeddings(self) -> None:
+        """
+        Function to visualise embedding space
+        """
+        self.bert.visualize_topics()
+
+        
