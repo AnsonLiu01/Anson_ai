728376b6d54ea46b4d2345b56de8d167aece84dd
AnsonLiu01 <ansonkliu@outlook.com>
2025-08-03 21:54:56 +0100

    changing input to cleaner document

--- src/__main__.py
+++ src/__main__.py
@@ -2,7 +2,7 @@ from topic_model.get_topics import GetTopics
 
 if __name__ == "__main__":
     topic_model = GetTopics(
-        transcript_loc='/Users/ansonliu/Documents/Github/Other/carl_rogers_therapy_sessions_gloria.pdf',
+        transcript_loc='/Users/ansonliu/Documents/Github/Other/transcripts/carl_rogers_therapy_sessions_gloria.docx',
         run_eda=True
     )
     
